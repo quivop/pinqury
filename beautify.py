e62f25dbf101a5c0d07e46a4d59961acb076167d
quivop <37252176+quivop@users.noreply.github.com>
2018-04-28 23:01:50 -0700

    Use a helpful code snippet to prettify html less stupidly

--- beautify.py
+++ beautify.py
@@ -1,5 +1,21 @@
 # beautify your new html file~
 from bs4 import BeautifulSoup
+import re
+
+
+# make a better prettifier~
+def my_prettifier(soup, indent_width=4, single_lines=True):
+    if single_lines:
+        for tag in soup():
+            tag.attrs = {
+                attr: [" ".join(attr_value.replace(
+                    "\n", " ").split()) for attr_value in value]
+                if isinstance(value, list)
+                else " ".join(value.replace("\n", " ").split())
+                for attr, value in tag.attrs.items()}
+
+    r = re.compile(r'^(\s*)', re.MULTILINE)
+    return r.sub(r'\1' * indent_width, soup.prettify())
 
 
 # grab the html~
@@ -11,7 +27,7 @@ ugly_html.close()
 parsed_html = BeautifulSoup(html_string, 'html.parser')
 
 # now stick it in a new file~
-pretty_html = open('htp/hallo.html', 'w')
+pretty_html = open('htp/hallo2.html', 'w')
 
-pretty_html.write(parsed_html.prettify())
+pretty_html.write(my_prettifier(parsed_html))
 pretty_html.close()

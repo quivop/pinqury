f2636bca12917d46e4cf17fc662c6c04b0c4f30c
quivop <37252176+quivop@users.noreply.github.com>
2018-04-28 23:38:20 -0700

    Use htmltidy to beautify away

--- beautify.py
+++ beautify.py
@@ -1,21 +1,5 @@
 # beautify your new html file~
-from bs4 import BeautifulSoup
-import re
-
-
-# make a better prettifier~
-def my_prettifier(soup, indent_width=4, single_lines=True):
-    if single_lines:
-        for tag in soup():
-            tag.attrs = {
-                attr: [" ".join(attr_value.replace(
-                    "\n", " ").split()) for attr_value in value]
-                if isinstance(value, list)
-                else " ".join(value.replace("\n", " ").split())
-                for attr, value in tag.attrs.items()}
-
-    r = re.compile(r'^(\s*)', re.MULTILINE)
-    return r.sub(r'\1' * indent_width, soup.prettify())
+from tidylib import tidy_document
 
 
 # grab the html~
@@ -24,10 +8,12 @@ html_string = ugly_html.read()
 ugly_html.close()
 
 # run it through a beautiful soup parser~
-parsed_html = BeautifulSoup(html_string, 'html.parser')
+parsed_html, errors = tidy_document(html_string)
 
 # now stick it in a new file~
-pretty_html = open('htp/hallo2.html', 'w')
+pretty_html = open('htp/hallo3.html', 'w')
 
-pretty_html.write(my_prettifier(parsed_html))
+pretty_html.write(parsed_html)
 pretty_html.close()
+
+print(errors)
